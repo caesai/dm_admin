d602a362faa0d4692b9c784259df48864f35984b
Vasily Kharlamov <caesai@yandex.ru>
2025-07-18 13:34:37 +0300

    Emoji Picker In Text Editor

--- src/components/TextEditor/Icons.tsx
+++ src/components/TextEditor/Icons.tsx
@@ -1,4 +1,4 @@
-export const Bold = ({ size = 16, color = "currentColor" }) => (
+export const Bold = ({ size = 16, color = 'currentColor' }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width={size}
@@ -13,9 +13,9 @@ export const Bold = ({ size = 16, color = "currentColor" }) => (
     <path d="M6 4h8a4 4 0 0 1 4 4 4 4 0 0 1-4 4H6z"></path>
     <path d="M6 12h9a4 4 0 0 1 4 4 4 4 0 0 1-4 4H6z"></path>
   </svg>
-);
+)
 
-export const Underline = ({ size = 16, color = "currentColor" }) => (
+export const Underline = ({ size = 16, color = 'currentColor' }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width={size}
@@ -30,9 +30,9 @@ export const Underline = ({ size = 16, color = "currentColor" }) => (
     <path d="M6 3v7a6 6 0 0 0 6 6 6 6 0 0 0 6-6V3"></path>
     <line x1="4" y1="21" x2="20" y2="21"></line>
   </svg>
-);
+)
 
-export const Italic = ({ size = 16, color = "currentColor" }) => (
+export const Italic = ({ size = 16, color = 'currentColor' }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width={size}
@@ -46,9 +46,9 @@ export const Italic = ({ size = 16, color = "currentColor" }) => (
   >
     <path d="M19 4h-9M14 20H5M14.7 4.7L9.2 19.4" />
   </svg>
-);
+)
 
-export const Link = ({ size = 16, color = "currentColor" }) => (
+export const Link = ({ size = 16, color = 'currentColor' }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width={size}
@@ -63,9 +63,9 @@ export const Link = ({ size = 16, color = "currentColor" }) => (
     <path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"></path>
     <path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"></path>
   </svg>
-);
+)
 
-export const Code = ({ size = 16, color = "currentColor" }) => (
+export const Code = ({ size = 16, color = 'currentColor' }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width={size}
@@ -80,9 +80,9 @@ export const Code = ({ size = 16, color = "currentColor" }) => (
     <polyline points="16 18 22 12 16 6"></polyline>
     <polyline points="8 6 2 12 8 18"></polyline>
   </svg>
-);
+)
 
-export const Strikethrough = ({ size = 16, color = "currentColor" }) => (
+export const Strikethrough = ({ size = 16, color = 'currentColor' }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width={size}
@@ -96,9 +96,9 @@ export const Strikethrough = ({ size = 16, color = "currentColor" }) => (
   >
     <path d="M17.3 4.9c-2.3-.6-4.4-1-6.2-.9-2.7 0-5.3.7-5.3 3.6 0 1.5 1.8 3.3 3.6 3.9h.2m8.2 3.7c.3.4.4.8.4 1.3 0 2.9-2.7 3.6-6.2 3.6-2.3 0-4.4-.3-6.2-.9M4 11.5h16" />
   </svg>
-);
+)
 
-export const RotateLeft = ({ size = 16, color = "currentColor" }) => (
+export const RotateLeft = ({ size = 16, color = 'currentColor' }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width={size}
@@ -112,9 +112,9 @@ export const RotateLeft = ({ size = 16, color = "currentColor" }) => (
   >
     <path d="M2.5 2v6h6M2.66 15.57a10 10 0 1 0 .57-8.38" />
   </svg>
-);
+)
 
-export const RotateRight = ({ size = 16, color = "currentColor" }) => (
+export const RotateRight = ({ size = 16, color = 'currentColor' }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width={size}
@@ -128,9 +128,9 @@ export const RotateRight = ({ size = 16, color = "currentColor" }) => (
   >
     <path d="M21.5 2v6h-6M21.34 15.57a10 10 0 1 1-.57-8.38" />
   </svg>
-);
+)
 
-export const X = ({ size = 16, color = "currentColor" }) => (
+export const X = ({ size = 16, color = 'currentColor' }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width={size}
@@ -145,4 +145,16 @@ export const X = ({ size = 16, color = "currentColor" }) => (
     <line x1="18" y1="6" x2="6" y2="18"></line>
     <line x1="6" y1="6" x2="18" y2="18"></line>
   </svg>
-);
+)
+
+export const Emoji = ({ size = 16, color = 'currentColor' }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={size}
+    height={size}
+    stroke={color}
+    viewBox="0 0 24 24"
+  >
+    <path d="M12 1a11 11 0 1 0 11 11A11.013 11.013 0 0 0 12 1zm0 20a9 9 0 1 1 9-9 9.011 9.011 0 0 1-9 9zm6-8a6 6 0 0 1-12 0 1 1 0 0 1 2 0 4 4 0 0 0 8 0 1 1 0 0 1 2 0zM8 10V9a1 1 0 0 1 2 0v1a1 1 0 0 1-2 0zm6 0V9a1 1 0 0 1 2 0v1a1 1 0 0 1-2 0z" />
+  </svg>
+)
