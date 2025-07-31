89cc9f4b42289504f966c9a2413ae3858c9820bb
kurudoqqe <vadim.firedog@gmail.com>
2025-07-31 21:17:43 +0300

    fix dark theme in notification page

--- src/views/notifications/NotificationPanels/OtherPanel.tsx
+++ src/views/notifications/NotificationPanels/OtherPanel.tsx
@@ -1,6 +1,6 @@
 import classNames from 'classnames'
 import {
-  CButton,
+  CButton, CCard,
   CTable,
   CTableBody,
   CTableDataCell,
@@ -17,31 +17,33 @@ const OtherPanel: FC<{
   texts: IText[]
 }> = ({ setTextId, texts }) => {
   return (
-    <CTabPanel itemKey="other" className={classNames('bg-white', 'p-3')}>
-      <CTable striped className={classNames('align-middle', 'table-hover', 'mb-0')}>
-        <CTableHead>
-          <CTableHeaderCell className={classNames('text-start', 'pb-3')}>Имя</CTableHeaderCell>
-          <CTableHeaderCell className={classNames('text-center', 'pb-3')}>Текст</CTableHeaderCell>
-          <CTableHeaderCell className={classNames('text-end', 'pe-2', 'pb-3')}>
-            Редактировать
-          </CTableHeaderCell>
-        </CTableHead>
-        <CTableBody className={classNames('border-top')}>
-          {texts.map((text) => (
-            <CTableRow key={text.id}>
-              <CTableDataCell className="text-start">{text.name}</CTableDataCell>
-              <CTableDataCell className="text-center">
-                {text.content ? text.content : 'Текст отсутствует'}
-              </CTableDataCell>
-              <CTableDataCell className={classNames('text-end', 'pe-0')}>
-                <CButton color={'primary'} onClick={() => setTextId(text.id)}>
-                  Редактировать
-                </CButton>
-              </CTableDataCell>
-            </CTableRow>
-          ))}
-        </CTableBody>
-      </CTable>
+    <CTabPanel itemKey="other">
+      <CCard className={classNames('p-3', 'border-0')}>
+        <CTable striped className={classNames('align-middle', 'table-hover', 'mb-0')}>
+          <CTableHead>
+            <CTableHeaderCell className={classNames('text-start', 'pb-3')}>Имя</CTableHeaderCell>
+            <CTableHeaderCell className={classNames('text-center', 'pb-3')}>Текст</CTableHeaderCell>
+            <CTableHeaderCell className={classNames('text-end', 'pe-2', 'pb-3')}>
+              Редактировать
+            </CTableHeaderCell>
+          </CTableHead>
+          <CTableBody className={classNames('border-top')}>
+            {texts.map((text) => (
+              <CTableRow key={text.id}>
+                <CTableDataCell className="text-start">{text.name}</CTableDataCell>
+                <CTableDataCell className="text-center">
+                  {text.content ? text.content : 'Текст отсутствует'}
+                </CTableDataCell>
+                <CTableDataCell className={classNames('text-end', 'pe-0')}>
+                  <CButton color={'primary'} onClick={() => setTextId(text.id)}>
+                    Редактировать
+                  </CButton>
+                </CTableDataCell>
+              </CTableRow>
+            ))}
+          </CTableBody>
+        </CTable>
+      </CCard>
     </CTabPanel>
   )
 }
