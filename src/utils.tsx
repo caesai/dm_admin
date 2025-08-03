65463cd1cfb7733cb920758287e4fc69e7236a82
sergantche <sergei.alex.kozlov@gmail.com>
2025-08-03 17:22:33 +0300

    create a single parser instance

--- src/utils.tsx
+++ src/utils.tsx
@@ -1,7 +1,8 @@
+const parser = new DOMParser()
+
 export const renderHTMLContent = (html: string | null) => {
   if (!html) return 'Текст отсутствует'
 
-  const parser = new DOMParser()
   const doc = parser.parseFromString(html, 'text/html')
   return <div dangerouslySetInnerHTML={{ __html: doc.body.innerHTML }} />
 }
