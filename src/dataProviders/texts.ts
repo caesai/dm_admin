0db0a28d5baa2ef45e9365fb2cf96815ef850ca2
kurudoqqe <vadim.firedog@gmail.com>
2025-09-03 23:48:36 +0300

    connect new greeting to api

--- src/dataProviders/texts.ts
+++ src/dataProviders/texts.ts
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import { BASEURL } from 'src/api.ts'
 import { IConfirmation, IText } from 'src/types/Texts.ts'
+import { IGreeting } from 'src/views/inviteLinks/Popups/NewGreetingPopup.tsx'
 
 export const getTextsList = async () => {
   return await axios.get<IText[]>(`${BASEURL}/texts/`, {
@@ -18,8 +19,24 @@ export const getTextById = async (id: number) => {
   })
 }
 
+export const getTextByName = async (name: string) => {
+  return await axios.get<IText>(`${BASEURL}/texts/by_name/${name}`, {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem('access_token')}`,
+    },
+  })
+}
+
 export const updateTextById = async (id: number, data: IText) => {
   return await axios.put(`${BASEURL}/texts/${id}`, data, {
+    headers: {
+      Authorization: 'Bearer ' + localStorage.getItem('access_token'),
+    },
+  })
+}
+
+export const updateTextByName = async (name: string, data: IGreeting) => {
+  return await axios.put(`${BASEURL}/texts/by_name/${name}`, data, {
     headers: {
       Authorization: 'Bearer ' + localStorage.getItem('access_token'),
     },
--- src/views/inviteLinks/Popups/NewGreetingPopup.tsx
+++ src/views/inviteLinks/Popups/NewGreetingPopup.tsx
@@ -7,14 +7,45 @@ import {
   CButton,
   CCard,
 } from '@coreui/react-pro'
-import { Dispatch, FC, SetStateAction, useState } from 'react'
+import { Dispatch, FC, SetStateAction, useEffect, useState } from 'react'
 import { TextEditor } from 'src/components/TextEditor/TextEditor.tsx'
+import { getTextByName, updateTextByName } from 'src/dataProviders/texts.ts'
+import toast from 'react-hot-toast'
+
+export interface IGreeting {
+  name: string
+  content: string
+}
 
 const NewGreetingPopup: FC<{
   popup: [boolean, Dispatch<SetStateAction<boolean>>]
 }> = ({ popup }) => {
   const [isActivePopup, setActivePopup] = popup
-  const [, setEditorContent] = useState<string>('')
+  const [editorContent, setEditorContent] = useState<string>('')
+  const [text, setText] = useState<IGreeting>({
+    name: 'start_default',
+    content: '',
+  })
+
+  const loadTexts = async () => {
+    const response = await getTextByName('start_default')
+    setText(response.data)
+    setEditorContent(response.data.content)
+  }
+
+  const updateText = async () => {
+    await updateTextByName('start_default', {
+      ...text,
+      content: editorContent,
+    })
+      .then(() => toast('Приветствие обновлено'))
+      .catch(() => toast.error('Ошибка обновления'))
+      .finally(() => setActivePopup(false))
+  }
+
+  useEffect(() => {
+    loadTexts()
+  }, [])
   return (
     <CModal
       size="lg"
@@ -27,14 +58,17 @@ const NewGreetingPopup: FC<{
       </CModalHeader>
       <CModalBody>
         <CCard>
-          <TextEditor onUpdate={setEditorContent} initialContent="Приветственный текст" />
+          <TextEditor
+            onUpdate={setEditorContent}
+            initialContent={editorContent === '' ? 'Приветственный текст' : editorContent}
+          />
         </CCard>
       </CModalBody>
       <CModalFooter className="flex-nowrap">
         <CButton color="secondary" className="w-100" onClick={() => setActivePopup(false)}>
           Отменить
         </CButton>
-        <CButton color="primary" className="w-100">
+        <CButton color="primary" className="w-100" onClick={updateText}>
           Сохранить
         </CButton>
       </CModalFooter>
