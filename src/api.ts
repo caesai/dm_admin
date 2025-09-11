15440993d639498d8ef99a5bb9bd9b4a85f27126
Vasily Kharlamov <caesai@yandex.ru>
2025-09-11 22:36:04 +0300

    Vite Config Fix

--- src/api.ts
+++ src/api.ts
@@ -1,8 +1,8 @@
-// @ts-expect-error env exist in meta....
+// @ts-ignore
 export const BASEURL = import.meta.env.MODE === 'development' ? 'https://devsoko.ru/admin'
   : 'https://backend.dreamteam.fm/admin'
 
-// @ts-expect-error env exist in meta....
+// @ts-ignore
 export const INVITE_LINK = import.meta.env.PROD
   ? 'https://t.me/dt_concierge_bot?start='
   : 'https://t.me/dmdev1bot?start='
