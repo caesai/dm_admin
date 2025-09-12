43ee3a323a6b1ed9fedab843509e40fcd05c05a3
kurudoqqe <vadim.firedog@gmail.com>
2025-09-12 15:35:01 +0300

    delete convert data to blob function

--- src/dataProviders/s3.ts
+++ src/dataProviders/s3.ts
@@ -17,26 +17,26 @@ export const uploadFile = async (file: File) => {
   })
 }
 
-var dataURLToBlob = function (dataURL: any) {
-  const BASE64_MARKER = ';base64,'
-  if (dataURL.indexOf(BASE64_MARKER) == -1) {
-    var parts = dataURL.split(',')
-    var contentType = parts[0].split(':')[1]
-    var raw = parts[1]
-
-    return new Blob([raw], { type: contentType })
-  }
-
-  var parts = dataURL.split(BASE64_MARKER)
-  var contentType = parts[0].split(':')[1]
-  var raw = window.atob(parts[1]) as any
-  const rawLength = raw.length
-
-  const uInt8Array = new Uint8Array(rawLength)
-
-  for (let i = 0; i < rawLength; ++i) {
-    uInt8Array[i] = raw.charCodeAt(i)
-  }
-
-  return new Blob([uInt8Array], { type: contentType })
-}
+// var dataURLToBlob = function (dataURL: any) {
+//   const BASE64_MARKER = ';base64,'
+//   if (dataURL.indexOf(BASE64_MARKER) == -1) {
+//     var parts = dataURL.split(',')
+//     var contentType = parts[0].split(':')[1]
+//     var raw = parts[1]
+//
+//     return new Blob([raw], { type: contentType })
+//   }
+//
+//   var parts = dataURL.split(BASE64_MARKER)
+//   var contentType = parts[0].split(':')[1]
+//   var raw = window.atob(parts[1]) as any
+//   const rawLength = raw.length
+//
+//   const uInt8Array = new Uint8Array(rawLength)
+//
+//   for (let i = 0; i < rawLength; ++i) {
+//     uInt8Array[i] = raw.charCodeAt(i)
+//   }
+//
+//   return new Blob([uInt8Array], { type: contentType })
+// }
