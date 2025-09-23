a592abc5b1a6ee7343d09876ce2ba3a283f98fb9
kurudoqqe <vadim.firedog@gmail.com>
2025-09-24 00:00:29 +0300

    change user booking page

--- src/views/users/UserPage.tsx
+++ src/views/users/UserPage.tsx
@@ -1,18 +1,14 @@
 import {
-  CBadge,
-  CButton,
   CCard,
   CCardBody,
   CCardHeader,
-  CCardImage,
-  CCardTitle,
   CCol,
-  CFormSelect,
   CRow,
   CSpinner,
   CTab,
   CTabContent,
   CTabList,
+  CTabPanel,
   // CTabPanel,
   CTabs,
 } from '@coreui/react-pro'
@@ -27,41 +23,25 @@ import {
 } from 'src/dataProviders/users.ts'
 import css from '../style/layout.module.css'
 import classNames from 'classnames'
-// import { UserEdit } from 'src/views/users/UserPageViews/UserEdit.tsx'
-// import { UserBookings } from 'src/views/users/UserPageViews/UserBookings.tsx'
-// import { UserReviews } from 'src/views/users/UserPageViews/UserReviews.tsx'
-// import { UserEvents } from 'src/views/users/UserPageViews/UserEvents.tsx'
-// import { UserPayments } from 'src/views/users/UserPageViews/UserPayments.tsx'
 import { IAdmin } from 'src/types/Admin.ts'
-import { getAdmins, setUserAdmin } from 'src/dataProviders/admins.ts'
-import toast from 'react-hot-toast'
+import { getAdmins } from 'src/dataProviders/admins.ts'
+import { UserEdit } from 'src/views/users/UserPageViews/UserEdit.tsx'
+import { UserBookings } from 'src/views/users/UserPageViews/UserBookings.tsx'
+import { IBookingWithRestaurant } from 'src/types/Booking.ts'
 
 const UserPage = () => {
   const { id } = useParams()
   const [user, setUser] = useState<IUserFull>()
+  const [bookings, setBookings] = useState<IBookingWithRestaurant[]>([])
   const [adminList, setAdminList] = useState<IAdmin[]>([])
 
   useEffect(() => {
     getUserById(Number(id)).then((res) => setUser(res.data))
     getUserLogs(Number(id)).then((res) => console.log('logs:', res.data))
-    getUserBookings(Number(id)).then((res) => console.log('bookings:', res.data))
+    getUserBookings(Number(id)).then((res) => setBookings(res.data))
     getUserEvents(Number(id)).then((res) => console.log('events:', res.data))
     getAdmins().then((res) => setAdminList(res.data))
   }, [])
-
-  const updateAdmin = (user: IUserFull) => {
-    if (!user.administrator) {
-      return
-    }
-    toast('Обработка')
-    setUserAdmin(user.id, user.administrator.id)
-      .then(() => toast.success('Сохранено'))
-      .catch(() => toast.error('Произошла ошибка при выполнении запроса.'))
-  }
-
-  const getBadge = (status: boolean) => {
-    return status ? 'success' : 'secondary'
-  }
 
   return !user ? (
     <CSpinner color="primary" />
@@ -75,91 +55,26 @@ const UserPage = () => {
           <CCardBody>
             <CRow>
               <div className={classNames(css.fr8, css.w100, css.mobileWrap)}>
-                <CCard style={{ width: '18rem', minWidth: '18rem' }}>
-                  <CCardImage orientation="top" src={user?.photo_url} />
-                  <CCardBody>
-                    <CCardTitle>{user.first_name}</CCardTitle>
-                    <div className={classNames(css.fc8)}>
-                      <span>Телефон: {user.phone_number}</span>
-                      <span>Email: {user.email}</span>
-                      <span>Регистрация: {new Date(user.created_at).toLocaleString()}</span>
-                      <span>Посл.редакт.: {new Date(user.updated_at).toLocaleString()}</span>
-                      <span>
-                        Ранний доступ.{' '}
-                        <CBadge color={getBadge(user.early_access)}>
-                          {user.early_access ? 'Да' : 'Нет'}
-                        </CBadge>
-                      </span>
-                      <span>
-                        Лиц. соглашение{' '}
-                        <CBadge color={getBadge(user.license_agreement)}>
-                          {user.license_agreement ? 'Да' : 'Нет'}
-                        </CBadge>
-                      </span>
-                      <span>
-                        Обработка данных{' '}
-                        <CBadge color={getBadge(user.gdpr_agreement)}>
-                          {user.gdpr_agreement ? 'Да' : 'Нет'}
-                        </CBadge>
-                      </span>
-                      <span>
-                        Рассылки{' '}
-                        <CBadge color={getBadge(user.advertisement_agreement)}>
-                          {user.advertisement_agreement ? 'Да' : 'Нет'}
-                        </CBadge>
-                      </span>
-                      {user ? (
-                        <div className={classNames(css.fc8, 'mt-4')}>
-                          <span>Администратор</span>
-                          <CFormSelect
-                            value={user.administrator ? String(user.administrator.id) : 'none'}
-                            onChange={(event) =>
-                              setUser((prev) => ({
-                                ...prev!,
-                                administrator: {
-                                  id: Number(event.target.value),
-                                  is_active: true,
-                                },
-                              }))
-                            }
-                          >
-                            <option value="none">Не администратор</option>
-                            {adminList.map((admin) => (
-                              <option key={admin.login} value={`${admin.id}`}>
-                                {admin.login}
-                              </option>
-                            ))}
-                          </CFormSelect>
-                          <CButton color={'primary'} onClick={() => updateAdmin(user)}>
-                            Сохранить
-                          </CButton>
-                        </div>
-                      ) : null}
-                    </div>
-                  </CCardBody>
-                </CCard>
                 <CCard className={classNames(css.w100)}>
                   <CCardHeader>
                     <strong>Информация</strong>
                   </CCardHeader>
                   <CCardBody>
-                    <CTabs activeItemKey="profile">
+                    <CTabs defaultActiveItemKey="home">
                       <CTabList variant="tabs">
-                        <CTab itemKey="home" defaultChecked={true}>
-                          Профиль
-                        </CTab>
-                        <CTab itemKey="bookings">Бронь</CTab>
+                        <CTab itemKey="home">Профиль</CTab>
+                        <CTab itemKey="bookings">Бронирование</CTab>
                         <CTab itemKey="events">Мероприятия</CTab>
                         <CTab itemKey={'reviews'}>Отзывы</CTab>
                         <CTab itemKey={'payments'}>Платежи</CTab>
                       </CTabList>
                       <CTabContent>
-                        {/*<CTabPanel className="p-3" itemKey="home">*/}
-                        {/*  <UserEdit user={user} />*/}
-                        {/*</CTabPanel>*/}
-                        {/*<CTabPanel className="p-3" itemKey="bookings">*/}
-                        {/*  <UserBookings user={user} />*/}
-                        {/*</CTabPanel>*/}
+                        <CTabPanel className="p-3" itemKey="home">
+                          <UserEdit userData={[user, setUser]} admins={[adminList, setAdminList]} />
+                        </CTabPanel>
+                        <CTabPanel className="p-3" itemKey="bookings">
+                          <UserBookings bookings={bookings} />
+                        </CTabPanel>
                         {/*<CTabPanel className="p-3" itemKey="events">*/}
                         {/*  <UserEvents user={user} />*/}
                         {/*</CTabPanel>*/}
--- src/views/users/UserPageViews/UserEdit.tsx
+++ src/views/users/UserPageViews/UserEdit.tsx
@@ -1,101 +1,185 @@
 import { IUserFull } from 'src/types/User.ts'
 import {
+  CBadge,
   CButton,
-  CCol,
-  CDatePicker,
-  CForm,
-  CFormCheck,
-  CFormInput,
-  CFormLabel,
-  CFormTextarea,
-  CInputGroup,
-  CInputGroupText,
-  CRow,
+  CCard,
+  CCardBody,
+  CCardImage,
+  CCardTitle,
+  CFormSelect,
 } from '@coreui/react-pro'
+import classNames from 'classnames'
+import css from 'src/views/style/layout.module.css'
+import toast from 'react-hot-toast'
+import { setUserAdmin } from 'src/dataProviders/admins.ts'
+import { Dispatch, SetStateAction } from 'react'
+import { IAdmin } from 'src/types/Admin.ts'
 
 interface Props {
-  user: IUserFull
+  userData: [IUserFull, Dispatch<SetStateAction<IUserFull | undefined>>]
+  admins: [IAdmin[], Dispatch<SetStateAction<IAdmin[]>>]
 }
 
-export const UserEdit = ({ user }: Props) => {
+export const UserEdit = ({ userData, admins }: Props) => {
+  const [user, setUser] = userData
+  const [adminList] = admins
+
+  const updateAdmin = (user: IUserFull) => {
+    if (!user.administrator) {
+      return
+    }
+    toast('Обработка')
+    setUserAdmin(user.id, user.administrator.id)
+      .then(() => toast.success('Сохранено'))
+      .catch(() => toast.error('Произошла ошибка при выполнении запроса.'))
+  }
+
+  const getBadge = (status: boolean) => {
+    return status ? 'success' : 'secondary'
+  }
   return (
-    <CForm className={'row g-3 needs-validation'}>
-      <CCol md={4}>
-        <CFormInput
-          type={'text'}
-          id={'first_name'}
-          defaultValue={user.first_name}
-          label={'Имя'}
-        ></CFormInput>
-      </CCol>
-      <CCol md={4}>
-        <CFormInput
-          type={'text'}
-          id={'last_name'}
-          defaultValue={user.last_name}
-          label={'Фамилия'}
-        ></CFormInput>
-      </CCol>
-      <CCol md={4}>
-        <CFormInput
-          type={'text'}
-          id={'email'}
-          defaultValue={user.email}
-          label={'Email'}
-        ></CFormInput>
-      </CCol>
-      <CCol md={4}>
-        <CFormLabel htmlFor="userFirstname">Логин Telegram</CFormLabel>
-        <CInputGroup>
-          <CInputGroupText>@</CInputGroupText>
-          <CFormInput
-            type="text"
-            id="validationCustomUsername"
-            disabled={true}
-            value={user.username}
-          />
-        </CInputGroup>
-      </CCol>
-      <CCol md={4}>
-        <CFormInput
-          type={'text'}
-          id={'phone_number'}
-          defaultValue={user.phone_number}
-          label={'Телефон'}
-        ></CFormInput>
-      </CCol>
-      <CCol md={4}>
-        <CFormCheck label={'Ранний доступ'} defaultChecked={user.early_access} />
-        <CFormCheck label={'Рассылки'} defaultChecked={user.advertisement_agreement} />
-        <CFormCheck label={'Лицензионное соглашение'} defaultChecked={user.license_agreement} />
-        <CFormCheck label={'Политика конфиденциальности'} defaultChecked={user.gdpr_agreement} />
-      </CCol>
-      <CCol md={4}>
-        <CFormInput
-          type={'text'}
-          id={'phone_number'}
-          defaultValue={user.phone_number}
-          label={'Телефон'}
-        ></CFormInput>
-      </CCol>
-      <CCol md={4}>
-        <CDatePicker label={'Дата рождения'} id={'date_of_birth'} date={user.date_of_birth} />
-      </CCol>
-      <CCol md={4}>
-        <CFormTextarea
-          id="allergies"
-          label="Аллергии"
-          rows={3}
-          defaultValue={user?.allergies}
-        ></CFormTextarea>
-      </CCol>
-      <CRow>
-        <CCol>
-          <CButton color={'primary'} type={'submit'}>
-            Обновить
-          </CButton>
-        </CCol>
-      </CRow>
-    </CForm>
+    <>
+      <CCard style={{ width: '18rem', minWidth: '18rem' }}>
+        <CCardImage orientation="top" src={user?.photo_url} />
+        <CCardBody>
+          <CCardTitle>{user.first_name}</CCardTitle>
+          <div className={classNames(css.fc8)}>
+            <span>Телефон: {user.phone_number}</span>
+            <span>Email: {user.email}</span>
+            <span>Регистрация: {new Date(user.created_at).toLocaleString()}</span>
+            <span>Посл.редакт.: {new Date(user.updated_at).toLocaleString()}</span>
+            <span>
+              Ранний доступ.{' '}
+              <CBadge color={getBadge(user.early_access)}>
+                {user.early_access ? 'Да' : 'Нет'}
+              </CBadge>
+            </span>
+            <span>
+              Лиц. соглашение{' '}
+              <CBadge color={getBadge(user.license_agreement)}>
+                {user.license_agreement ? 'Да' : 'Нет'}
+              </CBadge>
+            </span>
+            <span>
+              Обработка данных{' '}
+              <CBadge color={getBadge(user.gdpr_agreement)}>
+                {user.gdpr_agreement ? 'Да' : 'Нет'}
+              </CBadge>
+            </span>
+            <span>
+              Рассылки{' '}
+              <CBadge color={getBadge(user.advertisement_agreement)}>
+                {user.advertisement_agreement ? 'Да' : 'Нет'}
+              </CBadge>
+            </span>
+            {user ? (
+              <div className={classNames(css.fc8, 'mt-4')}>
+                <span>Администратор</span>
+                <CFormSelect
+                  value={user.administrator ? String(user.administrator.id) : 'none'}
+                  onChange={(event) =>
+                    setUser((prev) => ({
+                      ...prev!,
+                      administrator: {
+                        id: Number(event.target.value),
+                        is_active: true,
+                      },
+                    }))
+                  }
+                >
+                  <option value="none">Не администратор</option>
+                  {adminList.map((admin) => (
+                    <option key={admin.login} value={`${admin.id}`}>
+                      {admin.login}
+                    </option>
+                  ))}
+                </CFormSelect>
+                <CButton color={'primary'} onClick={() => updateAdmin(user)}>
+                  Сохранить
+                </CButton>
+              </div>
+            ) : null}
+          </div>
+        </CCardBody>
+      </CCard>
+      {/*<CForm className={'row g-3 needs-validation'}>*/}
+      {/*  <CCol md={4}>*/}
+      {/*    <CFormInput*/}
+      {/*      type={'text'}*/}
+      {/*      id={'first_name'}*/}
+      {/*      defaultValue={user.first_name}*/}
+      {/*      label={'Имя'}*/}
+      {/*    ></CFormInput>*/}
+      {/*  </CCol>*/}
+      {/*  <CCol md={4}>*/}
+      {/*    <CFormInput*/}
+      {/*      type={'text'}*/}
+      {/*      id={'last_name'}*/}
+      {/*      defaultValue={user.last_name}*/}
+      {/*      label={'Фамилия'}*/}
+      {/*    ></CFormInput>*/}
+      {/*  </CCol>*/}
+      {/*  <CCol md={4}>*/}
+      {/*    <CFormInput*/}
+      {/*      type={'text'}*/}
+      {/*      id={'email'}*/}
+      {/*      defaultValue={user.email}*/}
+      {/*      label={'Email'}*/}
+      {/*    ></CFormInput>*/}
+      {/*  </CCol>*/}
+      {/*  <CCol md={4}>*/}
+      {/*    <CFormLabel htmlFor="userFirstname">Логин Telegram</CFormLabel>*/}
+      {/*    <CInputGroup>*/}
+      {/*      <CInputGroupText>@</CInputGroupText>*/}
+      {/*      <CFormInput*/}
+      {/*        type="text"*/}
+      {/*        id="validationCustomUsername"*/}
+      {/*        disabled={true}*/}
+      {/*        value={user.username}*/}
+      {/*      />*/}
+      {/*    </CInputGroup>*/}
+      {/*  </CCol>*/}
+      {/*  <CCol md={4}>*/}
+      {/*    <CFormInput*/}
+      {/*      type={'text'}*/}
+      {/*      id={'phone_number'}*/}
+      {/*      defaultValue={user.phone_number}*/}
+      {/*      label={'Телефон'}*/}
+      {/*    ></CFormInput>*/}
+      {/*  </CCol>*/}
+      {/*  <CCol md={4}>*/}
+      {/*    <CFormCheck label={'Ранний доступ'} defaultChecked={user.early_access} />*/}
+      {/*    <CFormCheck label={'Рассылки'} defaultChecked={user.advertisement_agreement} />*/}
+      {/*    <CFormCheck label={'Лицензионное соглашение'} defaultChecked={user.license_agreement} />*/}
+      {/*    <CFormCheck label={'Политика конфиденциальности'} defaultChecked={user.gdpr_agreement} />*/}
+      {/*  </CCol>*/}
+      {/*  <CCol md={4}>*/}
+      {/*    <CFormInput*/}
+      {/*      type={'text'}*/}
+      {/*      id={'phone_number'}*/}
+      {/*      defaultValue={user.phone_number}*/}
+      {/*      label={'Телефон'}*/}
+      {/*    ></CFormInput>*/}
+      {/*  </CCol>*/}
+      {/*  <CCol md={4}>*/}
+      {/*    <CDatePicker label={'Дата рождения'} id={'date_of_birth'} date={user.date_of_birth} />*/}
+      {/*  </CCol>*/}
+      {/*  <CCol md={4}>*/}
+      {/*    <CFormTextarea*/}
+      {/*      id="allergies"*/}
+      {/*      label="Аллергии"*/}
+      {/*      rows={3}*/}
+      {/*      defaultValue={user?.allergies}*/}
+      {/*    ></CFormTextarea>*/}
+      {/*  </CCol>*/}
+      {/*  <CRow>*/}
+      {/*    <CCol>*/}
+      {/*      <CButton color={'primary'} type={'submit'}>*/}
+      {/*        Обновить*/}
+      {/*      </CButton>*/}
+      {/*    </CCol>*/}
+      {/*  </CRow>*/}
+      {/*</CForm>*/}
+    </>
   )
 }
