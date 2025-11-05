1bbadb1d952da61b2cfb80452832bf388dd2f3c0
kurudoqqe <vadim.firedog@gmail.com>
2025-11-05 04:37:12 +0300

    refactor bookings table

--- src/views/bookings/BookingsPage.tsx
+++ src/views/bookings/BookingsPage.tsx
@@ -1,15 +1,14 @@
 import { FC, useEffect, useState } from 'react'
 import { IBookingWithRestaurant } from 'src/types/Booking.ts'
 import { BookingPopup } from 'src/views/users/UserPageViews/Modals/BookingPopup.tsx'
-import { CCard, CCardBody, CCardGroup, CCardHeader, CSmartTable } from '@coreui/react-pro'
-import { Item } from '@coreui/react-pro/src/components/smart-table/types.ts'
-import { RestaurantCityCell } from 'src/views/users/UserPageViews/UserBookings.tsx'
+import { CCardGroup } from '@coreui/react-pro'
 import { getBookings } from 'src/dataProviders/bookings.ts'
 import toast from 'react-hot-toast'
 import classNames from 'classnames'
 import BookingsFilter from 'src/views/bookings/parts/BookingsFilter.tsx'
 import { GetRestaurantList } from 'src/dataProviders/restaurants.ts'
 import { IRestaurantWCity } from 'src/types/Restaurant.ts'
+import BookingsTable from 'src/views/bookings/parts/BookingsTable.tsx'
 
 const BookingsPage: FC = () => {
   const [bookings, setBookings] = useState<IBookingWithRestaurant[]>([])
@@ -40,112 +39,18 @@ const BookingsPage: FC = () => {
     loadRestaurants()
   }, [])
 
-  const cols = [
-    {
-      key: 'name',
-      label: 'Клиент',
-      _props: { scope: 'col' },
-    },
-    {
-      key: 'restaurant_name',
-      label: 'Ресторан',
-      _props: { scope: 'col' },
-    },
-    {
-      key: 'restaurant_id',
-      label: 'Город',
-      _props: { scope: 'col' },
-    },
-    {
-      key: 'booking_date',
-      label: 'Дата',
-      _props: { scope: 'col' },
-    },
-    {
-      key: 'time',
-      label: 'Время',
-      _props: { scope: 'col' },
-    },
-    {
-      key: 'duration',
-      label: 'Длительность',
-      _props: { scope: 'col' },
-    },
-    {
-      key: 'booking_status',
-      label: 'Статус',
-      _props: { scope: 'col' },
-    },
-    {
-      key: 'guests_count',
-      label: 'Гости',
-      _props: { scope: 'col' },
-    },
-    {
-      key: 'children_count',
-      label: 'Дети',
-      _props: { scope: 'col' },
-    },
-    {
-      key: 'remarked_comment',
-      label: 'Комментарий в Ремаркед',
-      _props: {
-        scope: 'col',
-        style: { width: '30%' },
-      },
-    },
-  ]
-
   return (
     <>
       <BookingPopup booking={[currentBooking, setCurrentBooking]} />
       <CCardGroup className={classNames('d-flex', 'flex-column', 'gap-4')}>
         <BookingsFilter restaurants={restaurants} setBookings={setBookings} />
-        <CCard className={classNames('rounded', 'border')}>
-          <CCardHeader>
-            <strong>Брони</strong>
-          </CCardHeader>
-          <CCardBody>
-            {bookings.length > 0 ? (
-              <CSmartTable
-                columns={cols}
-                items={bookings}
-                columnFilter
-                columnSorter
-                clickableRows
-                itemsPerPageSelect
-                itemsPerPage={itemsPerPage}
-                onItemsPerPageChange={setItemsPerPage}
-                itemsPerPageOptions={[10, 20, 50, 100]}
-                pagination
-                paginationProps={{
-                  pages: Math.ceil(totalItems / itemsPerPage),
-                  activePage: currentPage,
-                  onActivePageChange: setCurrentPage,
-                }}
-                onRowClick={(item: Item) => setCurrentBooking(item as IBookingWithRestaurant)}
-                tableHeadProps={{
-                  className: 'align-middle',
-                }}
-                tableProps={{
-                  striped: true,
-                  hover: true,
-                  className: 'align-middle',
-                }}
-                scopedColumns={{
-                  duration: (item: Item) => <td>{item.duration} мин</td>,
-                  restaurant_id: (item: Item) => (
-                    <RestaurantCityCell restaurantId={item.restaurant_id} />
-                  ),
-                }}
-              />
-            ) : (
-              <CCard>
-                <CCardBody>Бронирования не найдены, измените параметры поиска</CCardBody>
-              </CCard>
-            )}
-          </CCardBody>
-        </CCard>
+        <BookingsTable
+          bookings={bookings}
+          perPage={[itemsPerPage, setItemsPerPage]}
+          pageProps={[itemsPerPage, setCurrentPage]}
+          totalItems={totalItems}
+          setCurrentBooking={setCurrentBooking}
+        />
       </CCardGroup>
     </>
   )
