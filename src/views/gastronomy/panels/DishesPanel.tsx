1048a83bf84014e23670eb8c9f104f904cfaf70d
kurudoqqe <vadim.firedog@gmail.com>
2025-12-09 00:25:55 +0300

    add row onlcik popup in dishes panel

--- src/views/gastronomy/panels/DishesPanel.tsx
+++ src/views/gastronomy/panels/DishesPanel.tsx
@@ -3,6 +3,7 @@ import { CFormSelect, CSmartTable, CTabPanel } from '@coreui/react-pro'
 import classNames from 'classnames'
 import { Item } from '@coreui/react-pro/src/components/smart-table/types.ts'
 import toast from 'react-hot-toast'
+import { TablePopup } from 'src/components/TablePopup.tsx'
 import { getRestaurantDishes } from 'src/dataProviders/gastronomy.ts'
 import { ICulinaryDish } from 'src/types/Gastronomy.ts'
 import { getRestaurantCity } from 'src/utils.tsx'
@@ -13,6 +14,7 @@ const DishesPanel: FC = () => {
   const [dishes, setDishes] = useState<ICulinaryDish[]>([])
   const [restaurants, setRestaurants] = useState<IRestaurantWCity[]>([])
   const [currentId, setCurrentId] = useState<number>(0)
+  const [currentDish, setCurrentDish] = useState<ICulinaryDish | null>(null)
 
   const loadRestaurants = async () => {
     const response = await GetRestaurantList()
@@ -116,72 +118,84 @@ const DishesPanel: FC = () => {
   }, [currentId])
 
   return (
-    <CTabPanel itemKey={'dishes'} className={classNames('d-flex', 'flex-column', 'gap-1', 'py-3')}>
-      <CFormSelect
-        options={[
-          { label: 'Выберите ресторан', value: '' },
-          ...restaurants.map((restaurant) => {
-            const city = getRestaurantCity(restaurants, restaurant.id)
-            const address = restaurant.address || ''
-            const parts = [restaurant.title, city, address].filter(Boolean)
-            return {
-              label: parts.join(', '),
-              value: `${restaurant.id}`,
-            }
-          }),
-        ]}
-        value={currentId !== 0 ? String(currentId) : ''}
-        onChange={changeRestaurantId}
-      />
-      {dishes.length === 0 && currentId !== 0 ? (
-        <div className="text-center py-4">В ресторане нет новогодней кулинарии.</div>
-      ) : dishes.length > 0 ? (
-        <CSmartTable
-          columns={cols}
-          items={dishes}
-          tableHeadProps={{
-            className: 'align-middle',
-          }}
-          tableProps={{
-            striped: true,
-            hover: true,
-            className: classNames('align-middle', 'text-center'),
-          }}
-          scopedColumns={{
-            guest_title: (item: Item) => (
-              <td>{formatValue((item as ICulinaryDish).guest_title)}</td>
-            ),
-            image_url: (item: Item) => {
-              const dish = item as ICulinaryDish
-              return (
-                <td>
-                  {dish.image_url ? (
-                    <img
-                      src={dish.image_url}
-                      alt={'Блюдо'}
-                      style={{ maxWidth: '100px', maxHeight: '100px', objectFit: 'cover' }}
-                    />
-                  ) : (
-                    'Нет'
-                  )}
-                </td>
-              )
-            },
-            weight_volume_quantity: (item: Item) => (
-              <td>{getWeightVolumeQuantity(item as ICulinaryDish)}</td>
-            ),
-            weight_value: (item: Item) => (
-              <td>{formatValue((item as ICulinaryDish).weight_value)}</td>
-            ),
-            prices: (item: Item) => <td>{formatArray((item as ICulinaryDish).prices)}</td>,
-            allergens: (item: Item) => <td>{formatArray((item as ICulinaryDish).allergens)}</td>,
-            description: (item: Item) => (
-              <td>{formatValue((item as ICulinaryDish).description)}</td>
-            ),
-          }}
+    <>
+      <TablePopup data={[currentDish, setCurrentDish]} title={'Блюдо'} />
+      <CTabPanel
+        itemKey={'dishes'}
+        className={classNames('d-flex', 'flex-column', 'gap-1', 'py-3')}
+      >
+        <CFormSelect
+          options={[
+            { label: 'Выберите ресторан', value: '' },
+            ...restaurants.map((restaurant) => {
+              const city = getRestaurantCity(restaurants, restaurant.id)
+              const address = restaurant.address || ''
+              const parts = [restaurant.title, city, address].filter(Boolean)
+              return {
+                label: parts.join(', '),
+                value: `${restaurant.id}`,
+              }
+            }),
+          ]}
+          value={currentId !== 0 ? String(currentId) : ''}
+          onChange={changeRestaurantId}
         />
-      ) : null}
-    </CTabPanel>
+        {dishes.length === 0 && currentId !== 0 ? (
+          <div className="text-center py-4">В ресторане нет новогодней кулинарии.</div>
+        ) : dishes.length > 0 ? (
+          <CSmartTable
+            columns={cols}
+            items={dishes}
+            clickableRows
+            tableHeadProps={{
+              className: 'align-middle',
+            }}
+            tableProps={{
+              striped: true,
+              hover: true,
+              className: classNames('align-middle', 'text-center'),
+            }}
+            onRowClick={(item: Item) => setCurrentDish(item as ICulinaryDish)}
+            scopedColumns={{
+              id: (item: Item) => {
+                const index = dishes.findIndex((dish) => dish.id === (item as ICulinaryDish).id)
+                return <td>{index + 1}</td>
+              },
+              guest_title: (item: Item) => (
+                <td>{formatValue((item as ICulinaryDish).guest_title)}</td>
+              ),
+              image_url: (item: Item) => {
+                const dish = item as ICulinaryDish
+                return (
+                  <td>
+                    {dish.image_url ? (
+                      <img
+                        src={dish.image_url}
+                        alt={'Блюдо'}
+                        style={{ maxWidth: '100px', maxHeight: '100px', objectFit: 'cover' }}
+                      />
+                    ) : (
+                      'Нет'
+                    )}
+                  </td>
+                )
+              },
+              weight_volume_quantity: (item: Item) => (
+                <td>{getWeightVolumeQuantity(item as ICulinaryDish)}</td>
+              ),
+              weight_value: (item: Item) => (
+                <td>{formatValue((item as ICulinaryDish).weight_value)}</td>
+              ),
+              prices: (item: Item) => <td>{formatArray((item as ICulinaryDish).prices)}</td>,
+              allergens: (item: Item) => <td>{formatArray((item as ICulinaryDish).allergens)}</td>,
+              description: (item: Item) => (
+                <td>{formatValue((item as ICulinaryDish).description)}</td>
+              ),
+            }}
+          />
+        ) : null}
+      </CTabPanel>
+    </>
   )
 }
 
